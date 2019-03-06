0a59511ab340049565a5a616894d3ce7e8c8b23b
Sheraz Muzaffar <sharimalik52@gmail.com>
2019-03-07 00:41:31 +0500

    first commit

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -39,10 +39,17 @@ public class RulesOf6005 {
             boolean citingYourSource, boolean implementationRequired) {
         
         // TODO: Fill in this method, then remove the exception
-    	return true;
+    	 if(writtenByYourself==true && availableToOthers==false && writtenAsCourseWork==true && writtenAsCourseWork==true && citingYourSource==true && implementationRequired==true)
+          	return true;
+          else
+          	return false;
+     	
+     		
+     		
+     	}
+    	
         
 //        throw new RuntimeException("implement me!");
-    }
     
     /**
      * Main method of the class.
